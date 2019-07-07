6abf6d46c4dac742fa7590800ec9e2ac40038e69
Le Van Toan <letoan285@gmail.com>
2019-07-08 01:36:15 +0700

    add midleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,6 +15,9 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
